037af4c6575f98e8fd9fb31901c14191b7ee763d
Hristo Gospodinov <177312400+H-Gospodinov@users.noreply.github.com>
2025-04-25 12:43:18 +0300

    Task Manager: create working directory
    Create working directories and files for the "Task Manager" project.

---
+++ Task_Manager/src/index.js
@@ -0,0 +1,4 @@
+
+//import "./styles/styles.css"; //excluded to avoid FOUC
+import "./styles/media.css"; // include image css
+
---
+++ Task_Manager/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devtool: "source-map",
+  devServer: {
+    watchFiles: ["./src/html.html"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/html.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        exclude: /styles\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|svg|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
