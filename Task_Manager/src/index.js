e4ebf7624d3a56fb9afca0c0afdedf02622fb81b
Hristo Gospodinov <177312400+H-Gospodinov@users.noreply.github.com>
2025-04-27 14:13:42 +0300

    Task Manager: add event handlers
    Commit previously implemented event handlers.

--- Task_Manager/src/index.js
+++ Task_Manager/src/index.js
@@ -2,3 +2,34 @@
 //import "./styles/styles.css"; //excluded to avoid FOUC
 import "./styles/media.css"; // include image css
 
+import createGrid from "./modules/_grid.js";
+import {updateData} from "./modules/_data.js";
+
+const container = document.querySelector('#content')
+
+// (RE)CREATE GRID
+
+container.innerHTML = createGrid();
+
+document.addEventListener('dataChange', () => {
+    container.innerHTML = createGrid();
+});
+
+// EVENT HANDLERS
+
+container.addEventListener('click', (e) => {
+
+    const button = e.target;
+    const parent = button.closest('div');
+
+    switch(button.id) {
+
+        case 'edit':
+            updateData(null, parent);
+            break;
+
+        case 'delete':
+            updateData(null, null, parent);
+            break;
+    }
+});
