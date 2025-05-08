01c12ef277b431241c6086383c7bf1ab546b018a
Hristo Gospodinov <177312400+H-Gospodinov@users.noreply.github.com>
2025-05-08 12:13:37 +0300

    ESM demo: archive backup files
    Move the backup directory and files into a zip archive.

--- ESM_Backup/src/pages/infopage.js
+++
@@ -1,7 +0,0 @@
-import icon from "../img/logo.png";
-import createInfo from "./_textinfo.js";
-
-function createInfoPage() {
-    return `<img src="${icon}" width="150" alt="">` + createInfo();
-}
-export default createInfoPage;
