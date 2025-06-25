0400ae73176e5acf70ec992470a2b18b4018e439
Hristo Gospodinov <177312400+H-Gospodinov@users.noreply.github.com>
2025-06-25 14:46:09 +0300

    Battle Ship: create Player class
    Implement a factory function for player actions.

---
+++ Battleship/src/modules/player.js
@@ -0,0 +1,45 @@
+
+function Player(boardSize) {
+
+    return {
+
+        human(target) {
+
+            const index = target.dataset.id;
+
+            const x = index % boardSize;
+            const y = Math.floor(index / boardSize);
+
+            return {x, y};
+        },
+
+        computer() {
+
+            const strikes = new Set();
+            let x, y, key;
+
+            do {
+                x = Math.floor(Math.random() * boardSize);
+                y = Math.floor(Math.random() * boardSize);
+
+                key = `${x}, ${y}`;
+            }
+            while (strikes.has(key));
+
+            strikes.add(key); return {x, y};
+        },
+
+        attack(target, player) {
+
+            switch (player) {
+
+                case 'human':
+                    return this.human(target);
+
+                case 'computer':
+                    return this.computer();
+            }
+        },
+    };
+}
+export default Player;
