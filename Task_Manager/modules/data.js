80d4330ae65820655c0fdaf66e2e3411e614bbd2
Hristo Gospodinov <177312400+H-Gospodinov@users.noreply.github.com>
2025-09-30 01:07:07 +0300

    Task Manager: fix data filters
    Fix the "Upcoming" and "Overdue" filters,
    which have been broken after the previous commit.
    (the requreid currentTime function was removed by mistake)

--- Task_Manager/modules/data.js
+++ Task_Manager/modules/data.js
@@ -204,4 +204,18 @@ function updateData() {
             triggerEvent();
         },
     }
-} export {currentData, currentLabels, currentFilter, updateData};
+}
+// CURRENT TIME
+
+function currentTime() {
+
+    const date = new Date();
+
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+
+    return `${year}-${month}-${day}`;
+}
+
+export {currentData, currentLabels, currentFilter, updateData};
