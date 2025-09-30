a57c8eb2013cfb7f55c33f26bee7e8084711d33c
Hristo Gospodinov <177312400+H-Gospodinov@users.noreply.github.com>
2025-09-30 13:02:46 +0300

    Archive discarded projects
    Move the ESM and Jest projects into a zip archive.

--- ESM_Demo/src/pages/somepage.js
+++
@@ -1,7 +0,0 @@
-import icon from "../img/icon.png";
-import createText from "./_textbox.js";
-
-function createInfoPage() {
-    return `<img src="${icon}" width="150" alt="">` + createText();
-}
-export default createInfoPage;
